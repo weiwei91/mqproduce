104e040852ecb11cd48d1409f0ee0ffabacb169e
weiwei10 <weiwei10@hikvision.com>
2021-01-09 15:52:49 +0800

    kafka实例

--- src/main/java/com/mq/demo/SendMessageController.java
+++ src/main/java/com/mq/demo/SendMessageController.java
@@ -1,21 +1,8 @@
 package com.mq.demo;
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageDeliveryMode;
-import org.springframework.amqp.core.MessageProperties;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import com.mq.demo.service.IndicatorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.*;
+
 
 /**
  * @Author : JCccc
@@ -24,41 +11,14 @@ import java.util.UUID;
  **/
 @RestController
 public class SendMessageController {
-
-    //使用RabbitTemplate,这提供了接收/发送等等方法
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    IndicatorService indicatorService;
 
-    @Value("${mock.exchange}")
-    String exchange;
-    @Value("${mock.routingKey}")
-    String routingKey;
-    @Value("${mock.body}")
-    String body;
-
-    @Autowired
-    AmqpAdmin amqpAdmin;
-    @GetMapping("/create")
-    public String create() {
-        amqpAdmin.declareExchange(new DirectExchange("TestDirectExchange"));
-        amqpAdmin.declareQueue(new Queue("TestDirectQueue",true));
-        //创建绑定规则
-        amqpAdmin.declareBinding(new Binding("TestDirectQueue", Binding.DestinationType.QUEUE,"TestDirectExchange","TestDirectRouting",null));
-        System.out.println("创建完成");
+    @RequestMapping(value = "/sendMessage",method = RequestMethod.GET)
+    @ResponseBody
+    public String getCameras(@RequestParam(value = "name",required = false) String name) {
+        indicatorService.sendMessage("topic1","ceshi");
         return "ok";
     }
-
-
-    @GetMapping("/sendDirectMessage")
-    public String sendDirectMessage() {
-        MessageProperties properties = new MessageProperties();
-        properties.setDeliveryMode(MessageDeliveryMode.NON_PERSISTENT);
-        properties.setContentType("text/plain");
-        properties.setContentEncoding("UTF-8");
-        Message message = rabbitTemplate.getMessageConverter().toMessage(body, properties);
-        rabbitTemplate.convertAndSend(exchange, routingKey, body);
-        return "ok";
-    }
-
 
 }
